9d770436804428869657c0730768daf579c43d01
Mahmoud Hemaid <muui-1@hotmail.com>
2020-10-02 15:01:29 +0300

    Initial Commit

---
+++ LabAssigment1Tests/LabAssigment1Tests.swift
@@ -0,0 +1,33 @@
+//
+//  LabAssigment1Tests.swift
+//  LabAssigment1Tests
+//
+//  Created by Mahmoud Hemaid on 10/2/20.
+//
+
+import XCTest
+@testable import LabAssigment1
+
+class LabAssigment1Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
